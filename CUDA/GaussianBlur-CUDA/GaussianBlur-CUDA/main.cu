b5877355b2b6dc75144cf1ea3139b599e53cef6b
(s) Najim Mazidi <nmazidi>
2018-01-23 18:54:51 +0000

    input output, kernel working

--- CUDA/GaussianBlur-CUDA/GaussianBlur-CUDA/main.cu
+++ CUDA/GaussianBlur-CUDA/GaussianBlur-CUDA/main.cu
@@ -1,20 +1,25 @@
 #include "cuda_runtime.h"
+#include <cuda.h>
 #include "device_launch_parameters.h"
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
+#include <memory.h>
+#include <time.h>
 #include "lodepng.h"
 
-const int ker_x_dim = 3;
-const int ker_y_dim = 3;
+
+const int ker_x_dim = 1;
+const int ker_y_dim = 1;
 const double sigma = 1.0;
+//double *kernel = new double[(ker_x_dim * 2)*(ker_y_dim * 2)];
+double h_kernel[(ker_x_dim * 2)*(ker_y_dim * 2)];
+__constant__ double d_kernel[(ker_x_dim * 2)*(ker_y_dim * 2)];
 
-double *h_kernel = new double[(ker_x_dim * 2)*(ker_y_dim * 2)];
-__constant__ double *d_kernel = new double[(ker_x_dim * 2)*(ker_y_dim * 2)];
-
-void getGaussianKernel() {
+void getGaussianKernel() 
+{
 	// generate gaussian kernel values
-	double temp, sum = 0.0;
+	double temp = 0.0;
 	int r_i, r_j = 0;
 	for (int i = -ker_x_dim; i <= ker_x_dim; i++) {
 		r_i = i + ker_x_dim;
@@ -27,25 +32,62 @@ void getGaussianKernel() {
 	}
 }
 
+__host__ __device__ int get1dIndex(int width, int height, int x, int y)
+{
+	if (x < 0) x = 0;
+	if (x >= width) x = width - 1;
+	if (y < 0) y = 0;
+	if (y >= height) y = height - 1;
+	return y*width + x;
+}
+
 __global__ void runFilter(unsigned char* input, unsigned char* output, int width, int height) {
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
-	const unsigned int offset = blockIdx.x*blockDim.x + threadIdx.x;
-	int x = offset % width;
-	int y = (offset - x) / width;
 	float r, g, b = 0;
 	int count = 0;
-	for (int i = -ker_x_dim; i < ker_x_dim; ++i) {
-		for (int j = -ker_y_dim; j < ker_y_dim; ++j) {
-			if ((x + i) >= 0 && (x + i) < width && (y + j) >= 0 && (y + j) < height) {
-				const int idx = (offset + i + j * width) * 3;
-				r += d_kernel[]
+	if (row < width && col < height) {
+		for (int i = -ker_x_dim; i < ker_x_dim; i++) {
+			for (int j = -ker_y_dim; j < ker_y_dim; j++) {
+				int idx = get1dIndex(width, height, row + i, col + j);
+				
 			}
 		}
+	
+		float r, g, b = 0.0f;
+		for (int i = 0; i < ker_x_dim; i++) {
+			
+		}
 	}
+
+	printf("%f,%f,%f\n", r,g,b);
+
 }
 
-int main() {
+void convolveImage(float* input, float* output, int width, int height) 
+{
+	unsigned char* d_input;
+	unsigned char* d_output;
+	cudaMalloc(&d_input, width*height * sizeof(float));
+	cudaMalloc(&d_output, width*height * sizeof(float));
+	cudaMemcpyToSymbol(d_kernel, h_kernel, sizeof(h_kernel));
+	cudaMemcpy(d_input, input, width*height * sizeof(double), cudaMemcpyHostToDevice);
+
+	dim3 blockDim(25, 25, 1);
+	dim3 gridDim((unsigned int) ceil((double)(width*height*3/blockDim.x)), 1, 1);
+	printf("height: %d, width: %d", height, width);
+
+	runFilter << <gridDim, blockDim >> >(d_input, d_output, width, height);
+	cudaError_t cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		printf("Error: %s\n", cudaGetErrorString(cudaStatus));
+
+	}
+	cudaDeviceSynchronize();
+}
+
+int main(int argc, int** argv) 
+{
 	const char* image_path = "image.png";
 	const char* output_path = "output.png";
 
@@ -59,48 +101,40 @@ int main() {
 	if (error) {
 		printf("decoder error: %d, %s", error, lodepng_error_text(error));
 	}
-	unsigned char* input = new unsigned char[(img_vect.size() * 3) / 4];
-	unsigned char* output = new unsigned char[(img_vect.size() * 3) / 4];
+	int image_size = width*height; 
+	float* input;
+	float* output;
+	cudaMallocHost(&input, (image_size*3) * sizeof(float));
+	cudaMallocHost(&output, (image_size*3) * sizeof(float));
 	int count = 0;
-	
 	// getting rid of the apha channel as it is not needed
 	for (int i = 0; i < img_vect.size(); ++i) {
 		if ((i + 1) % 4 != 0) {
 			input[count] = img_vect.at(i);
-			output[count] = 255;
 			count++;
 		}
 	}
-	//printf("%d, %d, %d\n", input[0], input[1], input[2]);
-	unsigned char* d_inPixels;
-	unsigned char* d_outPixels;
-	cudaMalloc(&d_inPixels, width*height * sizeof(float));
-	cudaMalloc(&d_outPixels, width*height * sizeof(float));
-	cudaMemcpyToSymbol(d_kernel, h_kernel, sizeof(float));
-	cudaMemcpy(d_inPixels, input, width*height * sizeof(float), cudaMemcpyHostToDevice);
 
-	dim3 blockDim(16, 16, 1);
-	dim3 gridDim(width / blockDim.x + 1, height / blockDim.y + 1);
+	clock_t tStart = clock();
+	convolveImage(input, output, width, height);
+	clock_t tEnd = clock();
+	float ms = 1000.0f * (tEnd - tStart) / CLOCKS_PER_SEC;
+	printf("Convolution took %fms.\n", ms);
 
-	runFilter<<<gridDim, blockDim>>>(input, output, width, height);
-	
-
-	// Prepare data for output
-	/*std::vector<unsigned char> out_image;
-	printf("test");
-	for (int i = 0; i < img_vect.size(); ++i) {
-		out_image.push_back(output[i]);
+	std::vector<unsigned char> out_image;
+	unsigned char temp;
+	for (int i = 0; i < image_size * 3; i++) {
+		out_image.push_back(input[i]);
 		if ((i + 1) % 3 == 0) {
 			out_image.push_back(255);
 		}
-	}*/
-
-	// Output the data
-	//error = lodepng::encode(output_path, out_image, width, height);
+	}
+	error = lodepng::encode(output_path, out_image, width, height);
 
 	//if there's an error, display it
-	//if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-
+	if (error) {
+		printf("%s", lodepng_error_text(error));
+	}
 	
 }
 
